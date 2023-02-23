84d63ea33dc19f8504234bd94b6ccbd3a1a1ecd0
Nikita-Babich <nikita.babich.a@gmail.com>
2023-02-24 00:31:37 +0300

    added len
    and some comments, because working on one thing is incredibly slow. I usually write down ideas as they appear in different parts of the program, so i am constantly progressing. But if i have to commit to "commit" structure, i easily get stuck on one thing.

--- set.cpp
+++ set.cpp
@@ -9,12 +9,32 @@ int* a = (int*) malloc(100 * sizeof(int));
 int* b = (int*) malloc(100 * sizeof(int));
 int* res = (int*) malloc(100 * sizeof(int));
 
-void intersect(){
+int len(int* array){
+	int size = sizeof(array)/sizeof(array[0]);
+	return size;
 }
+
+//void read_set1(){
+//	int read_index = 0;
+//	bool cont = true;
+//	if(id=="a"){
+//		for(int i=0; cont; i++){
+//			scanf("%d", )
+//		}
+//	}
+//}
+
+void intersect(int* a, int* b){
+	//sizeof()
+	//res = realloc(res, newSize);
+}
+
+
 
 void unite(){
 }
 
 main(){
-	
+	//read_set1();
+	//read_set2();
 }
