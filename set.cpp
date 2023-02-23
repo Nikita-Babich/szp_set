70ebc8a260cd28e4634953da15cef0a44a054e2b
Nikita-Babich <nikita.babich.a@gmail.com>
2023-02-24 00:15:54 +0300

    allocation
    testing the creation of dynamic arrays

--- set.cpp
+++ set.cpp
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+//new pointers
+int* a = (int*) malloc(100 * sizeof(int));
+int* b = (int*) malloc(100 * sizeof(int));
+int* res = (int*) malloc(100 * sizeof(int));
+
 void intersect(){
 }
 
