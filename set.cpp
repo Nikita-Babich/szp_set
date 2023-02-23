b81e23251db7cc5c1c8dfa96bed44eda67286eaa
Nikita-Babich <nikita.babich.a@gmail.com>
2023-02-24 02:04:59 +0300

    +reset +shrink +unite
    final version for now

--- set.cpp
+++ set.cpp
@@ -39,7 +39,24 @@ void print_res(int* z){
 	printf("\n");
 }
 
+void reset(int* z){
+	for(int i=0;i<20;i++){ z[i]=0;}
+}
+
+void shrink(int* z){ //check for repetitions and delete them
+	for(int i=0;i<19;i++){
+		if(z[i]!=0){
+			for(int j=i+1;j<20;j++){
+				if(z[i]==z[j]){
+					z[j]=0;
+				}
+			}
+		}
+	}
+}
+
 void intersect(int* a, int* b, int* z){
+	reset(z); //reset
 	int index = 0;
 	for(int i=0;i<10;i++){
 		if(inside(a[i],b)){
@@ -50,10 +67,17 @@ void intersect(int* a, int* b, int* z){
 	for(;index<10;index++){
 		z[index]=0;
 	}
+	shrink(z);
 }
 
 void unite(int* a, int* b, int* z){
-	
+	for(int i=0;i<10;i++){
+		z[i]=a[i];
+	}
+	for(int i=0;i<10;i++){
+		z[i+10]=b[i];
+	}
+	shrink(z);
 }
 
 main(){
@@ -64,10 +88,17 @@ main(){
 	srand(time(0));
 	generate(a);
 	generate(b);
+	
+	printf("sets original");
 	print_array(a);
 	print_array(b);
 	
 	intersect(a,b,res);
+	printf("intersection");
+	print_res(res);
+	
+	unite(a,b,res);
+	printf("unity");
 	print_res(res);
 }
 
